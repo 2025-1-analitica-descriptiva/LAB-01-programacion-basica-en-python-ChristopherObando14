6987b143b482ef65b52bb9a3ade7eb57037ac20e
Christopher Obando <cobandor@unal.edu.co>
2025-05-13 13:32:25 -0500

    Solución 3

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -15,3 +15,21 @@ def pregunta_03():
     [('A', 53), ('B', 36), ('C', 27), ('D', 31), ('E', 67)]
 
     """
+    with open("files/input/data.csv", "r") as archivo:
+        conteo_letras = {}
+        for linea in archivo:
+            columnas = linea.strip().split('\t')
+            if columnas:
+                letra = columnas[0]
+                valor = int(columnas[1])
+                if letra in conteo_letras:
+                    conteo_letras[letra] += valor
+                else:
+                    conteo_letras[letra] = valor
+
+    resultado = [(letra, suma) for letra, suma in conteo_letras.items()]
+    resultado.sort(key=lambda x: x[0])
+    return resultado
+
+
+
