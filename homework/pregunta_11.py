a78ad681e6f13d51659b29ff2bfb1d94271e7438
Christopher Obando <cobandor@unal.edu.co>
2025-05-13 15:46:25 -0500

    Solución 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -16,3 +16,19 @@ def pregunta_11():
 
 
     """
+    resultado = {}
+
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            columnas = linea.strip().split('\t')
+            valor_col2 = int(columnas[1])
+            pares_col4 = columnas[3].split(',')
+
+            for par in pares_col4:
+                letra = par.split(':')[0]
+                if letra in resultado:
+                    resultado[letra] += valor_col2
+                else:
+                    resultado[letra] = valor_col2
+
+    return dict(sorted(resultado.items()))
